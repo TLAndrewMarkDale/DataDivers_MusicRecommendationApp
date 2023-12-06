758f0da24192ac5e369b773cdb11faa6726e9ba0
TLAndrewMarkDale <andrew.dale@dcmail.ca>
2023-12-06 12:52:04 -0500

    Update flask_NN.py
    Updated searchbar

--- music-recommendation-backend/flask_NN.py
+++ music-recommendation-backend/flask_NN.py
@@ -17,7 +17,7 @@ search_df = music_df[['track_id', 'artists', 'track_name']].copy()
 def send_searchbar():
     search_criteria = request.get_data()
     search_criteria = search_criteria.decode()
-    found_df = search_df[search_df['artists'].str.contains(search_criteria) or search_df['track_name'].str.contains(search_criteria)]
+    found_df = search_df[search_df['artists'].str.contains(search_criteria) | search_df['track_name'].str.contains(search_criteria)]
     found_df = found_df.head(20)
     for index, song in found_df.iterrows():
         json_array.append({'track_id': song['track_id'],
@@ -51,7 +51,7 @@ def send_topten():
 def send_recommendations():
     track_id = request.get_data()
     track_id = track_id.decode()
-    nbrs = neigh.kneighbors(training_df.iloc[music_df.loc[music_df['track_id'] == track_id].index[0]].values.reshape(1,-1), n_neighbors=11, return_distance=False)
+    nbrs = neigh.kneighbors(training_df.iloc[music_df.loc[music_df['track_id'] == track_id].index[0]].values.reshape(1,-1), n_neighbors=51, return_distance=False)
     json_array = []
     for nbr in nbrs[0][1:]:
         row = music_df.iloc[nbr]
